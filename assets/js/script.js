3f5c761954474b1234a0e20cfded32706a1c4235
M Rafi Andi Prayitno <rafiandiprayitno9528@gmail.com>
2025-01-26 00:18:28 +0700

    finish web portofolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
 const navbarList = document.querySelector('.list')
-        const navbarMenu = document.querySelector('.menu')
+const navbarMenu = document.querySelector('.menu')
 
-        navbarMenu.addEventListener('click',  () =>  {
-            navbarList.classList.toggle('aktif')
-        })
+navbarMenu.addEventListener('click',  () =>  {
+    navbarList.classList.toggle('aktif')
+})
 
-        document.addEventListener('click', function(e) {
-            if(!navbarList.contains(e.target) && !navbarMenu.contains(e.target)){
-                navbarList.classList.remove('aktif')
-            }
-        })
+document.addEventListener('click', function(e) {
+    if(!navbarList.contains(e.target) && !navbarMenu.contains(e.target)){
+        navbarList.classList.remove('aktif')
+    }
+})
+
+const sertifAll = document.querySelectorAll('.sertif')
+sertifAll.forEach(sertif => {
+    sertif.addEventListener('mouseover', () => {
+        const deskripsi = sertif.querySelector('.deskripsi')
+        
+        if(deskripsi){
+            deskripsi.style.opacity = '80%';
+        }
+    })
+    sertif.addEventListener('mouseout', () => {
+        const deskripsi = sertif.querySelector('.deskripsi')
+        
+        if(deskripsi){
+            deskripsi.style.opacity = '0';
+        }
+    })
+});
